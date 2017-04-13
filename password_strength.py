eebe097da750edb48c9639369f4620c07c523fb4
userok <admin@1rpi.ru>
2017-04-13 14:09:33 +0500

    список исправлений в описании
    1. Изменены название функций; 2. использование with open; 3. использование getpass; 4. Очки были размазаны по всех программы и перенесены вверхней части кода.

--- password_strength.py
+++ password_strength.py
@@ -1,63 +1,50 @@
 import os
+from getpass import getpass
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-def check_black_list(password):
-    '''
-    Если пароля нет в blacklist вернуть значение 1
-    иначе 0
-    '''
+
+SCORE_BLACK_LIST = 1
+SCORE_DIGIT = 1
+SCORE_LOWERCASE_AND_UPPERCASE = 2
+SCORE_SPECIAL_CHAR = 2
+SCORE_LENGTH = 4
+
+def get_score_black_list(password):
     score = 0
-    f = open('data/black_list.txt')
-    black_list = map(lambda item: item.strip(), f.readlines())
-    f.close()
-    
-    if all(password != item for item in black_list): 
-        score = 1
+    with open('data/black_list.txt') as f:
+        black_list = map(lambda item: item.strip(), f.readlines())
+        if all(password != item for item in black_list): 
+            score = SCORE_BLACK_LIST
+            
     return score
 
-def check_digit(password):
-    '''
-    Если есть цифра в пароле вернуть 1
-    иначе 0
-    '''
+def get_score_digit(password):
     score = 0
     if any(char.isdigit() for char in password): 
-        score = 1
+        score = SCORE_DIGIT
     return score
 
-def check_lower_and_upper(password):
-    '''
-    Если есть нижний и верхний регистр вернуть 2
-    иначе 0
-    '''
+def get_score_lowercase_and_uppercase(password):
     score = 0
     if any( char.isupper() for char in password) and \
        any(char.islower() for char in password): 
-        score =  2
+        score = SCORE_LOWERCASE_AND_UPPERCASE
     return score
 
 
-def check_special_char(password):
-    '''
-    Если есть спец символ, то вернуть 2
-    иначе 0
-    '''
+def get_score_special_char(password):
     score = 0
     special_characters = '!@#$%^&*()_+.,:;'
     if any(char in special_characters for char in password): 
-        score = 2
+        score = SCORE_SPECIAL_CHAR
     return score
 
 
-def check_length(password):
-    '''
-    Если длина пароля от 12 вернуть 4
-    иначе 0
-    '''
+def get_score_length(password):
     score = 0
     if len(password) >= 12:
-        score = 4
+        score = SCORE_LENGTH
     return score
 
 def get_password_strength(password):
@@ -65,19 +52,17 @@ def get_password_strength(password):
     if len(password) == 0: # Если пароль пуст 0 баллов
         return score
 
-    score += check_black_list(password)
-    score += check_digit(password)
-    score += check_lower_and_upper(password)
-    score += check_special_char(password)
-    score += check_length(password)
+    score += get_score_black_list(password)
+    score += get_score_digit(password)
+    score += get_score_lowercase_and_uppercase(password)
+    score += get_score_special_char(password)
+    score += get_score_length(password)
 
     return score
 
 
-
-
 if __name__ == '__main__':
     print('Программа. Сложность пароля.\n')
-    answer = input('Введите пароль: ')
+    answer = getpass(prompt='Введите пароль: ')
     password_strength = get_password_strength(answer)
     print('Оценка сложности Вашего пароля: {}'.format(password_strength))
